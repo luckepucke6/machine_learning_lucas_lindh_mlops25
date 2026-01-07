89b5d59ad30350855fd99802f057a32ca38cee5b
Lucas Lindh <lucas.lindh6@live.se>
2026-01-07 13:20:31 +0100

    create

--- code_alongs/08_fastapi_crud/api.py
+++ code_alongs/08_fastapi_crud/api.py
@@ -6,13 +6,28 @@ books: list[Book] = library_data("library.json").books
 
 app = FastAPI()
 
+# ----------------------- READ -----------------------
+
+
 @app.get("/books")
 async def read_books():
     # can return a pydantic model because
     # fastapi serializes it to json under the hood
     return books
 
+
 # path parameter
 @app.get("/book/{id}")
 async def read_book_by_id(id: int):
     return [book for book in books if book.id == id]
+
+
+# ----------------------- CREATE -----------------------
+
+@app.post("/books/create_book")
+async def create_book(book_request: Book):
+    new_book = Book.model_validate(book_request)
+
+    books.append(new_book)
+
+    return new_book
