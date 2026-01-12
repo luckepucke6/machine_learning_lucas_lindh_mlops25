ac02f1fb8a0b0708efa7275878639dfd28462f3b
Lucas Lindh <lucas.lindh6@live.se>
2026-01-12 10:46:56 +0100

    fastapi_data start

---
+++ code_alongs/10_fastapi_data/data_processing.py
@@ -0,0 +1,22 @@
+from constants import DATA_PATH
+import pandas as pd
+from pprint import pprint
+import json
+
+df = pd.read_csv(DATA_PATH / "Sales.csv")
+
+class DataExplorer:
+    def __init__(self, limit = 100):
+        self._df = df.head(limit)
+
+    @property
+    def df(self):
+        return self._df    
+    
+    def json_response(self):
+        json_data = self.df.to_json(orient="records")
+        return json.loads(json_data)
+
+if __name__ == "__main__":
+    data_explorer = DataExplorer(limit=2)
+    pprint(data_explorer.json_response())
