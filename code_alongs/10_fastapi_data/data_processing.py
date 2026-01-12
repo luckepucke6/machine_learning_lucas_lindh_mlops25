80faf4cf7c67013c14ca239e4c9d1f4fa2d8370b
Lucas Lindh <lucas.lindh6@live.se>
2026-01-12 14:26:24 +0100

    fastapi_data

--- code_alongs/10_fastapi_data/data_processing.py
+++ code_alongs/10_fastapi_data/data_processing.py
@@ -5,18 +5,40 @@ import json
 
 df = pd.read_csv(DATA_PATH / "Sales.csv")
 
+
 class DataExplorer:
-    def __init__(self, limit = 100):
+    def __init__(self, limit=100):
         self._df = df.head(limit)
+        self._df_full = df
 
     @property
     def df(self):
-        return self._df    
+        return self._df
+
+    def summary(self):
+        self._df = (
+            self._df_full.describe()
+            .T.drop(["count"], axis=1)
+            .drop(["Day", "Year"])
+            .reset_index()
+        )
+        return self  # allows fot method chaining
     
+    
+    def kpis(self, country: str):
+        df_by_country = df.query("Country.str.casefold() == @country.casefold()")
+
+        return {
+            "total_profit": str(df_by_country["Profit"].sum()),
+            "total_cost": str(df_by_country["Cost"].sum()),
+            "number_of_purchases": str(len(df_by_country))
+        }
+
     def json_response(self):
         json_data = self.df.to_json(orient="records")
         return json.loads(json_data)
 
+
 if __name__ == "__main__":
     data_explorer = DataExplorer(limit=2)
-    pprint(data_explorer.json_response())
+    pprint(data_explorer.summary().json_response())
